dd931cd696ece0d00ff76051fc93618c49698197
Matthijs <Mattie112@users.noreply.github.com>
2024-10-27 17:07:01 +0100

    WIP on #24

--- main.go
+++ main.go
@@ -88,7 +88,10 @@ func main() {
 }
 
 func loadConfig() sConfig {
-	return sConfig{allRocketLaunches: getenvBool("ALL_ROCKET_LAUNCHES")}
+	return sConfig{
+		allRocketLaunches: getenvBool("ALL_ROCKET_LAUNCHES"),
+		achievementMode:   getenvBool("ACHIEVEMENT_MODE"),
+	}
 }
 
 // Parse the message and format it in a way for Discord
@@ -223,7 +226,12 @@ func sendMessageToFactorio(rconClient *rcon.Client) {
 	log.Debugf("Setting up message handler")
 	for message := range messagesToFactorio {
 		message = strings.Replace(message, "'", "\\'", -1)
-		cmd := "/silent-command game.print('[color=#7289DA][Discord]" + message + "[/color]')"
+		cmd := ""
+		if config.achievementMode {
+			cmd = "[color=#7289DA][Discord]" + message + "[/color]"
+		} else {
+			cmd = "/silent-command game.print('[color=#7289DA][Discord]" + message + "[/color]')"
+		}
 		log.WithFields(logrus.Fields{"cmd": cmd}).Debug("Sending command to Factorio (through RCON)")
 		_, err := rconClient.Execute(cmd)
 		if err != nil {
@@ -432,4 +440,5 @@ func checkRequiredEnvVariables() {
 
 type sConfig struct {
 	allRocketLaunches bool
+	achievementMode   bool
 }
